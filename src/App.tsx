67283490ce8945116283331dc94dbb10111bbe3b
David Gaspar <davsangas@hotmail.com>
2023-10-10 21:34:42 -0600

    Feat: Animation and fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import { motion } from "framer-motion";
 import QR from './components/QR';
 
 function App() {
@@ -9,22 +10,29 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center font-sans">
+    <div className="flex flex-col items-center justify-center font-sans">
       <img
         className="relative w-screen h-screen object-cover"
-        src="https://cdn.pixabay.com/photo/2016/04/15/04/02/water-1330252_1280.jpg"
+        src="/src/assets/wallpaperflare.com_wallpaper.jpg"
         alt=""
       />
-      <div className="absolute flex flex-col items-center mt-4 rounded-lg border w-3/4 h-1/2 backdrop:blur-sm">
-        <h1 className="bg-gradient-to-r from-blue-700 mt-4 via-blue-800 to-gray-900 text-transparent text-3xl font-bold font-sans bg-clip-text">
+      <div className="absolute flex flex-col items-center mt-4 rounded-lg border border-gray-200 w-3/4 h-1/2 backdrop-blur-lg lg:w-1/4 lg:h-1/2">
+        <h1 className="font-bold text-3xl text-sky-200 font-sans py-4">
           QR Generator
         </h1>
-          <input type="text" value={url} onChange={handleChange} placeholder="Introduce el link" className="p-2 rounded bg-transparent text-black border-2 mb-2"/>
-        <footer className="p-1.5 mt-20 flex items-center justify-center">
+          <input type="text" value={url} onChange={handleChange} placeholder="Introduce el link" className="p-2 rounded bg-transparent text-gray-200 border-2 mb-2 focus:border-gray-300"/>
+        <footer className="p-1.5 mt-5 flex items-center justify-center">
           {
-            url.length > 0
-            ?<QR url={url} />
-            :<p className="text-gray-500">Introduce un link para generar el QR</p>
+            url.length > 0 ? (
+              <motion.div
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{duration: 1}}
+                >
+                  <QR url={url} />
+                </motion.div>
+            )
+            : <p className="text-gray-300">Introduce un link para generar el QR</p>
           }
         </footer>
       </div>
